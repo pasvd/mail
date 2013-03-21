75a2dc3c0afbc34b448b1c41631be785631aacb3
Jiri Prochazka <jiri.prochazka@kyberie.cz>
2013-03-21 13:23:25 +0100

    catch err begin/rescue

--- lib/mail/fields/unstructured_field.rb
+++ lib/mail/fields/unstructured_field.rb
@@ -146,7 +146,10 @@ module Mail
         line = ""
         while !words.empty?
           break unless word = words.first.dup
-          word.encode!(charset) if charset && word.respond_to?(:encode!)
+          begin
+            word.encode!(charset) if charset && word.respond_to?(:encode!)
+          rescue
+          end
           word = encode(word) if should_encode
           word = encode_crlf(word)
           # Skip to next line if we're going to go past the limit
